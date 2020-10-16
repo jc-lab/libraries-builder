06e45181d0ce68000452480f63a5a9735f249689
Jichan <development@jc-lab.net>
2020-10-16 15:22:31 +0900

    improve for use

--- cmake/FindProtobuf.cmake
+++ cmake/FindProtobuf.cmake
@@ -1,3 +1,9 @@
+if (COMMON_LIBRARY_ROOT_STATIC)
+    set(_TEMP_LIBRARY_DIR_PREFIX ${COMMON_LIBRARY_ROOT_STATIC})
+else()
+    set(_TEMP_LIBRARY_DIR_PREFIX ${BUILDING_INSTALL_PREFIX})
+endif()
+
 set(protobuf_MODULE_COMPATIBLE ON)
-include(${BUILDING_INSTALL_PREFIX}/lib/cmake/protobuf/protobuf-config.cmake)
+include(${_TEMP_LIBRARY_DIR_PREFIX}/lib/cmake/protobuf/protobuf-config.cmake)
 set(Protobuf_FOUND TRUE)
--- cmake/FindgRPC.cmake
+++ cmake/FindgRPC.cmake
@@ -1,2 +1,8 @@
-include(${BUILDING_INSTALL_PREFIX}/lib/cmake/grpc/gRPCConfig.cmake)
+if (COMMON_LIBRARY_ROOT_STATIC)
+    set(_TEMP_LIBRARY_DIR_PREFIX ${COMMON_LIBRARY_ROOT_STATIC})
+else()
+    set(_TEMP_LIBRARY_DIR_PREFIX ${BUILDING_INSTALL_PREFIX})
+endif()
+
+include(${_TEMP_LIBRARY_DIR_PREFIX}/lib/cmake/grpc/gRPCConfig.cmake)
 set(gRPC_FOUND TRUE)
