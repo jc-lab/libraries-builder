2bca623169726e490761105387e32629f1a60a40
Jichan <development@jc-lab.net>
2020-10-16 11:02:48 +0900

    feat: add msvc toolset version into dist directory name

--- builder/build-all.bat
+++ builder/build-all.bat
@@ -2,8 +2,10 @@ set CURRENT_DIR=%~dp0
 set PROJECT_DIR=%CURRENT_DIR%..
 set TOP_BUILD_DIR=%CURRENT_DIR%..\build\
 set CLEAN_PATH=%PATH%
+set TOP_DIST_DIR=%TOP_BUILD_DIR%dist\
 
 mkdir %TOP_BUILD_DIR%
+mkdir %TOP_DIST_DIR%
 
 set _EXIT_CODE=0
 
@@ -13,11 +15,9 @@ rem ==================================================
 set PATH=%CLEAN_PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-
 set BUILD_ARCH=Win32
 set BUILD_CONFIG=Debug
 set BUILD_DIR=%TOP_BUILD_DIR%build-windows-%BUILD_ARCH%-%BUILD_CONFIG%
-set DIST_DIR=%TOP_BUILD_DIR%dist\windows-%BUILD_ARCH%-%BUILD_CONFIG%
 echo "Build start: %BUILD_ARCH%-%BUILD_CONFIG%"
 call %CURRENT_DIR%\win-build-one.bat
 
@@ -27,7 +27,6 @@ if %_EXIT_CODE% neq 0 goto BAT_EXIT
 set BUILD_ARCH=Win32
 set BUILD_CONFIG=RelWithDebInfo
 set BUILD_DIR=%TOP_BUILD_DIR%build-windows-%BUILD_ARCH%-%BUILD_CONFIG%
-set DIST_DIR=%TOP_BUILD_DIR%dist\windows-%BUILD_ARCH%-%BUILD_CONFIG%
 echo "Build start: %BUILD_ARCH%-%BUILD_CONFIG%"
 call %CURRENT_DIR%\win-build-one.bat
 
@@ -44,7 +43,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 set BUILD_ARCH=x64
 set BUILD_CONFIG=Debug
 set BUILD_DIR=%TOP_BUILD_DIR%build-windows-%BUILD_ARCH%-%BUILD_CONFIG%
-set DIST_DIR=%TOP_BUILD_DIR%dist\windows-%BUILD_ARCH%-%BUILD_CONFIG%
 echo "Build start: %BUILD_ARCH%-%BUILD_CONFIG%"
 call %CURRENT_DIR%\win-build-one.bat
 
@@ -55,7 +53,6 @@ if %_EXIT_CODE% neq 0 goto BAT_EXIT
 set BUILD_ARCH=x64
 set BUILD_CONFIG=RelWithDebInfo
 set BUILD_DIR=%TOP_BUILD_DIR%build-windows-%BUILD_ARCH%-%BUILD_CONFIG%
-set DIST_DIR=%TOP_BUILD_DIR%dist\windows-%BUILD_ARCH%-%BUILD_CONFIG%
 echo "Build start: %BUILD_ARCH%-%BUILD_CONFIG%"
 call %CURRENT_DIR%\win-build-one.bat
 
--- builder/win-build-one.bat
+++ builder/win-build-one.bat
@@ -9,6 +9,10 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build . --config %BUILD_CONFIG%
 rem if %errorlevel% neq 0 exit /b %errorlevel%
 
+set /p BUILD_MSVC_TOOLSET_VERSION=< %BUILD_DIR%\msvc-toolset-version.txt
+
+set DIST_DIR=%TOP_DIST_DIR%windows-%BUILD_ARCH%-msvc-v%BUILD_MSVC_TOOLSET_VERSION%-%BUILD_CONFIG%
+
 cmake --install . --config %BUILD_CONFIG% --prefix %DIST_DIR%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
